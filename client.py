8dfeeeee4a55ed05aa0296b4619f8c1abf496f78
Ande Luiz <andreplacet@gmail.com>
2023-05-04 22:26:41 -0300

    fix:bot permission to voice

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import os
 
 class DiscordClient:
-    def __init__(self, intents):
-        self.client = commands.Bot(command_prefix='!', intents=intents)
+    def __init__(self, intents, permissions):
+        self.client = commands.Bot(command_prefix='!', intents=intents, permissions=permissions)
 
     def get_client(self):
         return self.client
